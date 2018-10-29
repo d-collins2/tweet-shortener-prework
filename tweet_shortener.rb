5f456531c5cbfe38e4a56c4fe0c1be4a079668b9
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-29 09:49:44 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,42 @@
 # Write your code here.
+# dictionary = {
+# "hello" => 'hi',
+# "to, two, too" => '2', 
+# "for, four" => '4',
+# 'be' => 'b',
+# 'you' => 'u',
+# "at" => "@", 
+# "and" => "&"
+# }
+
+def word_substituter(tweets)
+  dictionary = {
+"hello" => 'hi',
+"to" => '2',  
+"two" => '2', 
+"too" => '2', 
+"for" => '4',
+"four" => '4',
+'be' => 'b',
+'you' => 'u',
+"at" => "@", 
+"and" => "&"
+}
+  p string = tweets.split
+  array = []
+  string.each do |word| 
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+      array << word 
+    else 
+      array << word 
+    end 
+  end
+  array.join(" ")
+end 
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+  puts word_substituter(tweet)
+end 
+end 
